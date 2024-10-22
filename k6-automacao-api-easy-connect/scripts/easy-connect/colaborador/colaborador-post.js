d58ee68cfd4cf9592c6da0af8573d9841bcbdc9b
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2024-10-22 19:34:30 -0300

    fix

---
+++ k6-automacao-api-easy-connect/scripts/easy-connect/colaborador/colaborador-post.js
@@ -0,0 +1,191 @@
+import http from "k6/http";
+import { expect } from "https://jslib.k6.io/k6chaijs/4.3.4.3/index.js";
+import {
+  colaborador,
+  criarColaboradorOpcionaisPreenchidosNulo as opcoesNulas,
+  colaboradorCPFInvalido as colaboradorCpfErrado,
+  colaboradorResponsavelCPFOmitido,
+  criarColaboradorOmitindoCNPJCPF as cnpjcpffaltando,
+  criarColaboradorOmitindoNome as nomefaltando,
+  criarColaboradorOmitindoCPF as cpffaltando,
+  criarColaboradorOmitindoCodigoMunicipio as codigoMunicipioFaltando,
+  criarColaboradorOmitindoCodigoJornada as codigoJornada,
+} from "./postColaborador.js";
+import {
+  makeRequest,
+  checkStatus,
+  checkErrorInList,
+  checkErrorMessage,
+} from "../utils.js/utils.js";
+import { describe } from "https://jslib.k6.io/expect/0.0.5/index.js";
+import { schema } from "../colaborador/colaborador-schema.js";
+
+const token = `${__ENV.API_TOKEN}`;
+const baseUrl = `${__ENV.BASE_URL}`;
+
+export let options = {
+  thresholds: {
+    //falhar o teste se qualquer verificação falhar ou se qualquer requisição falhar
+    checks: [{ threshold: "rate == 1.00", abortOnFail: true }],
+    //http_req_failed: [{ threshold: "rate == 0.00", abortOnFail: true }],
+  },
+  vus: 1,
+  iterations: 1,
+};
+
+var returnAPI;
+export function criarColaborador() {
+  describe("Payload completo com todos os campos obrigatórios preenchidos corretamente.", (t) => {
+    const endpoint = `${baseUrl}/colaborador/create`;
+    const res = makeRequest("POST", endpoint, colaborador, token);
+
+    console.log(res.body);
+
+    returnAPI = res;
+    checkStatus(201, res.status);
+  });
+}
+
+function validateSchemaColaborador() {
+  describe("Create a new funcionario", (t) => {
+    t.expect(returnAPI)
+      .toHaveValidJson()
+      .and(returnAPI.json())
+      .toMatchAPISchema(schema);
+  });
+}
+
+export function criarColaboradorComOpcoesNulas() {
+  describe("Campos Opcionais Preenchidos com Nulo", () => {
+    const endpoint = `${baseUrl}/colaborador/create`;
+    const res = makeRequest("POST", endpoint, opcoesNulas, token);
+    checkStatus(201, res.status);
+  });
+}
+
+export function criarColaboradorComCPFInvalido() {
+  describe("Criar colaborador com CPF inválido", () => {
+    const endpoint = `${baseUrl}/colaborador/create`;
+    const res = makeRequest("POST", endpoint, colaboradorCpfErrado, token);
+
+    checkStatus(400, res.status);
+
+    // Parseia o corpo da resposta
+    const resBodyJson = JSON.parse(res.body);
+
+    checkStatus("400", resBodyJson.status_code);
+    checkErrorMessage("Dados inválidos", resBodyJson.mensagem);
+    checkErrorInList("Mínimo de caracteres no CPF é 11", resBodyJson.errors);
+  });
+}
+
+export function criarColaboradorOmitindoCPFResponsavel() {
+  describe("Criação de colaborador com campo CPF Responsável faltando", () => {
+    const endpoint = `${baseUrl}/colaborador/create`;
+    const res = makeRequest(
+      "POST",
+      endpoint,
+      colaboradorResponsavelCPFOmitido,
+      token
+    );
+
+    checkStatus(400, res.status);
+
+    // Parseia o corpo da resposta
+    const resBodyJson = JSON.parse(res.body);
+
+    checkStatus("400", resBodyJson.status_code);
+    checkErrorMessage("Dados inválidos", resBodyJson.mensagem);
+    checkErrorInList("O CPF do responsável é obrigatório", resBodyJson.errors);
+  });
+}
+
+export function criarColaboradorOmitindoCNPJCPF() {
+  describe("Criação de colaborador com campo CNPJCPF faltando", () => {
+    const endpoint = `${baseUrl}/colaborador/create`;
+    const res = makeRequest("POST", endpoint, cnpjcpffaltando, token);
+
+    checkStatus(400, res.status);
+
+    // Parseia o corpo da resposta
+    const resBodyJson = JSON.parse(res.body);
+
+    checkStatus("400", resBodyJson.status_code);
+    checkErrorMessage("Dados inválidos", resBodyJson.mensagem);
+    checkErrorInList("O CNPJ/CPF é obrigatório", resBodyJson.errors);
+  });
+}
+
+export function criarColaboradorOmitindoNome() {
+  describe("Criação de colaborador com campo nome faltando", () => {
+    const endpoint = `${baseUrl}/colaborador/create`;
+    const res = makeRequest("POST", endpoint, nomefaltando, token);
+
+    checkStatus(400, res.status);
+
+    // Parseia o corpo da resposta
+    const resBodyJson = JSON.parse(res.body);
+
+    checkStatus("400", resBodyJson.status_code);
+    checkErrorMessage("Dados inválidos", resBodyJson.mensagem);
+    checkErrorInList("O nome é obrigatório", resBodyJson.errors);
+  });
+}
+
+export function criarColaboradorOmitindoCPF() {
+  describe("Criação de colaborador com campo CPF faltando", () => {
+    const endpoint = `${baseUrl}/colaborador/create`;
+    const res = makeRequest("POST", endpoint, cpffaltando, token);
+
+    checkStatus(400, res.status);
+
+    // Parseia o corpo da resposta
+    const resBodyJson = JSON.parse(res.body);
+
+    checkStatus("400", resBodyJson.status_code);
+    checkErrorMessage("Dados inválidos", resBodyJson.mensagem);
+    checkErrorInList("O CPF é obrigatório", resBodyJson.errors);
+  });
+}
+
+export function criarColaboradorOmitindoCodigoMunicipio() {
+  describe("Criação de colaborador com campo CodigoMunicipio faltando", () => {
+    const endpoint = `${baseUrl}/colaborador/create`;
+    const res = makeRequest("POST", endpoint, codigoMunicipioFaltando, token);
+
+    checkStatus(400, res.status);
+
+    const resBodyJson = JSON.parse(res.body);
+
+    checkStatus("400", resBodyJson.status_code);
+    checkErrorMessage("Dados inválidos", resBodyJson.mensagem);
+    checkErrorInList("O código do município é obrigatório", resBodyJson.errors);
+  });
+}
+
+export function criarColaboradorOmitindoCodigoJornada() {
+  describe("Criação de colaborador com campo CodigoJornada faltando", () => {
+    const endpoint = `${baseUrl}/colaborador/create`;
+    const res = makeRequest("POST", endpoint, codigoJornada, token);
+
+    checkStatus(400, res.status);
+
+    const resBodyJson = JSON.parse(res.body);
+
+    checkStatus("400", resBodyJson.status_code);
+    checkErrorMessage("Dados inválidos", resBodyJson.mensagem);
+    checkErrorInList("O código", resBodyJson.errors);
+  });
+}
+
+export default function testSuite() {
+  criarColaborador();
+  validateSchemaColaborador();
+  criarColaboradorComOpcoesNulas();
+  criarColaboradorComCPFInvalido();
+  criarColaboradorOmitindoCPFResponsavel();
+  criarColaboradorOmitindoCNPJCPF();
+  criarColaboradorOmitindoNome();
+  criarColaboradorOmitindoCPF();
+  criarColaboradorOmitindoCodigoMunicipio();
+}
---
+++ k6-automacao-api-easy-connect/scripts/easy-connect/utils.js/utils.js
@@ -0,0 +1,67 @@
+import http from "k6/http";
+import {
+  describe,
+  expect,
+} from "https://jslib.k6.io/k6chaijs/4.3.4.3/index.js";
+
+export function makeRequest(method, endpoint, payload, token) {
+  const jsonPayload = JSON.stringify(payload);
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      "api-easy-token": token,
+    },
+  };
+
+  switch (method.toLowerCase()) {
+    case "post":
+      return http.post(endpoint, jsonPayload, options);
+    case "put":
+      return http.put(endpoint, jsonPayload, options);
+    default:
+    // throw new Error(Método ${method} não suportado);
+  }
+}
+
+export function checkStatus(expectedStatus, actualStatus) {
+  // Alterado o nome do parâmetro
+  if (actualStatus === expectedStatus) {
+    expect(
+      actualStatus,
+      `O status code esperado é ${expectedStatus}. O status code retornado foi ${actualStatus}`
+    ).to.equal(expectedStatus);
+  } else {
+    expect(
+      actualStatus,
+      `O status code esperado é ${expectedStatus}. Entretanto, o status code retornado foi: ${actualStatus}`
+    ).to.equal(expectedStatus);
+  }
+}
+
+export function checkErrorMessage(expectedMessage, actualMessage) {
+  if (actualMessage === expectedMessage) {
+    expect(
+      actualMessage,
+      `A mensagem de erro esperada é "${expectedMessage}". A mensagem retornada foi "${actualMessage}".`
+    ).to.equal(expectedMessage);
+  } else {
+    expect(
+      actualMessage,
+      `A mensagem de erro esperada é "${expectedMessage}". Entretanto, a mensagem retornada foi: "${actualMessage}".`
+    ).to.equal(expectedMessage);
+  }
+}
+
+export function checkErrorInList(expectedError, actualErrorsList) {
+  if (actualErrorsList.includes(expectedError)) {
+    expect(
+      actualErrorsList,
+      `A lista de erros contém o erro esperado: "${expectedError}".`
+    ).to.include(expectedError);
+  } else {
+    expect(
+      actualErrorsList,
+      `A lista de erros deveria conter o erro esperado: "${expectedError}", mas não contém.`
+    ).to.include(expectedError);
+  }
+}
