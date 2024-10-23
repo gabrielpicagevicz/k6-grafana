abf892ae7efb89755d31a613f1f817d7b47a92db
Gabriel Picagevicz <gabrielpicagevicz15@gmail.com>
2024-10-22 22:44:16 -0300

    tomra  q vai

--- k6-automacao-api-easy-connect/scripts/easy-connect/colaborador/colaborador-post.js
+++ k6-automacao-api-easy-connect/scripts/easy-connect/colaborador/colaborador-post.js
@@ -37,8 +37,11 @@ var returnAPI;
 export function criarColaborador() {
   describe("Payload completo com todos os campos obrigatórios preenchidos corretamente.", (t) => {
 
-    const endpoint = `${__ENV.BASE_URL}/colaborador/create`;
-    const res = makeRequest("POST", endpoint, colaborador,  `${__ENV.BASE_URL}`);
+    //const endpoint = `${__ENV.BASE_URL}/colaborador/create`;
+
+    http.post(`http://${__ENV.BASE_URL}/colaborador/create`);
+
+    //const res = makeRequest("POST", endpoint, colaborador,  `${__ENV.BASE_URL}`);
 
     console.log(res.body);
 
